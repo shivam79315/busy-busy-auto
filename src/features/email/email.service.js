237dc911fc5a920dbed4f2d08f8b49c9a3ccfff9
shivam <shivam79315@gmail.com>
2026-03-16 00:30:36 +0530

    Updated mailer to googleapis

--- src/features/email/email.service.js
+++ src/features/email/email.service.js
@@ -1,12 +1,11 @@
-import { transporter } from "../../config/mailer.js";
+import { sendMail } from "../../config/mailer.js";
 import { orderConfirmationTemplate } from "./email.template.js";
 
 export const sendOrderConfirmationEmail = async (payload) => {
 
   const template = orderConfirmationTemplate(payload);
 
-  await transporter.sendMail({
-    from: `"Busy Busy Store" <${process.env.GMAIL_USER}>`,
+  await sendMail({
     to: payload.email,
     subject: template.subject,
     html: template.html
