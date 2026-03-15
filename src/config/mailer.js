2d96f8b189b3e9a47454987aeed8a857bb819bdb
shivam <shivam79315@gmail.com>
2026-03-16 00:30:36 +0530

    Updated mailer to googleapis

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,11 +1,45 @@
-import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: true,
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS
-  }
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.GMAIL_CLIENT_ID,
+  process.env.GMAIL_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN
 });
+
+export const sendMail = async ({ to, subject, html }) => {
+
+  const gmail = google.gmail({
+    version: "v1",
+    auth: oauth2Client
+  });
+
+  const messageParts = [
+    `From: Busy-busy <${process.env.GMAIL_USER}>`,
+    `To: ${to}`,
+    `Subject: ${subject}`,
+    "Content-Type: text/html; charset=utf-8",
+    "",
+    html
+  ];
+
+  const message = messageParts.join("\n");
+
+  const encodedMessage = Buffer.from(message)
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+
+  await gmail.users.messages.send({
+    userId: "me",
+    requestBody: {
+      raw: encodedMessage
+    }
+  });
+};
